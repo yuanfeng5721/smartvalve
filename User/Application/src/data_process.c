51f7ed41bfefa1f7f3d763d2e74dabec7107536b
wangkun <wangkun5721@163.com>
2022-02-15 09:19:06 +0800

    1. 修改mqtt clientid和username的长度 2. 增加不同sample freq和update freq的处理流程 3. 增加valve control的流程

--- User/Application/src/data_process.c
+++ User/Application/src/data_process.c
@@ -21,6 +21,9 @@
 #include "rtc_wakeup.h"
 
 #define DATA_PROCESS_TASK_PRIORITY      (osPriorityNormal+1)
+#define IS_UPDATE_SENSOR_DATA(sample_count, sample_freq, update_freq) (((sample_count+1)%(update_freq/sample_freq)) == 0)
+#define RECORD_SENSOR_ITEM(sample_count, sample_freq) (sample_count%(60/sample_freq))
+#define MAX_SAMPLE_ITEM(sample_freq) (24*60/sample_freq)
 
 osThreadId dataProcessTaskHandle;
 osMessageQId dataProcessQueueHandle;
@@ -43,6 +46,15 @@ dp_t dp_data[DP_MAX_NUMBER] = {
 		{.name = "overflow",}
 };
 
+#define D_CLIENT_ID "TEST001"
+#define D_USERNAME  "448403"
+#define D_PASSWD    "WIZpdLCOQv1cWYCbrKmwYzM5XeJ6ewZ9Ly0M7lrQLgs="
+
+static char clientid[20];
+static char username[20];
+static char passwd[50];
+static UpdateFreq update_freq = 5;
+static SampleFreq sample_freq = 5;
 static cavan_json_t report_json;
 static uint32_t msg_id = 0;
 
@@ -72,9 +84,59 @@ static uint32_t msg_id = 0;
 //        ]
 //    }
 //}
+void init_mqtt_account(void)
+{
+	memset(clientid, 0, sizeof(clientid));
+	memset(username, 0, sizeof(username));
+	memset(passwd, 0, sizeof(passwd));
+
+	strcpy(clientid, nvitem_get_string(NV_CLIENT_ID));
+	strcpy(username, nvitem_get_string(NV_USERNAME));
+	strcpy(passwd, nvitem_get_string(NV_PASSWORD));
+
+	LOGD("client id: %s\r\n", clientid);
+	LOGD("user name: %s\r\n", username);
+	LOGD("pass word: %s\r\n", passwd);
+}
+
+void init_device_params(void)
+{
+	update_freq = CHECK_UPDATE_FREQ(nvitem_get_int(UPDATE_FREQ));
+	sample_freq = CHECK_SAMPLE_FREQ(nvitem_get_int(SAMPLE_FREQ));
+
+	LOGD("update_freq = %d min\r\n",update_freq);
+	LOGD("sample_freq = %d min\r\n",sample_freq);
+}
+
+void sensor_data_clear(uint8_t num, uint8_t item)
+{
+	//for(int i=0; i<DP_MAX_NUMBER; i++) {
+	if(num>=0 && num<DP_MAX_NUMBER) {
+		if(item>=0 && item<STROE_MAX_NUMBER) {
+			dp_data[num].dt[item].v = 0.0;
+			dp_data[num].dt[item].t = 0;
+			dp_data[num].dt[item].isOk = false;
+		}
+	}
+	//}
+}
+
+void sensor_data_clear_all(void)
+{
+	for(int i=0; i<DP_MAX_NUMBER; i++) {
+		for(int j=0; j<STROE_MAX_NUMBER; j++) {
+			dp_data[i].dt[j].v = 0.0;
+			dp_data[i].dt[j].t = 0;
+			dp_data[i].dt[j].isOk = false;
+		}
+	}
+	//}
+}
 cavan_json_t *make_report_json(void)
 {
-	uint8_t i = 0;
+	uint8_t i = 0, j = 0;
+	uint8_t need_dot = 0;
+
 	cavan_json_t *json = &report_json;
 
 	cavan_json_init(json);
@@ -88,16 +150,25 @@ cavan_json_t *make_report_json(void)
 	{
 		cavan_json_append_name(json, dp_data[i].name);
 		cavan_json_array_begin(json);
-		cavan_json_begin(json);
-		cavan_mqtt_append_float2(json, "v", dp_data[i].dt[0].v);
-		cavan_mqtt_append_int2(json, "t", dp_data[i].dt[0].t);
-		cavan_json_end(json);
+		for(j=0; j<STROE_MAX_NUMBER; j++)
+		{
+			if(dp_data[i].dt[j].isOk)
+			{
+				if(need_dot++){
+					cavan_json_append(json, ',');
+				}
+				cavan_json_begin(json);
+				cavan_mqtt_append_float2(json, "v", dp_data[i].dt[j].v);
+				cavan_mqtt_append_int2(json, "t", dp_data[i].dt[j].t);
+				cavan_json_end(json);
+				sensor_data_clear(i, j);
+			}
+		}
 		cavan_json_array_end(json);
 	}
 	cavan_json_end(json);
 	cavan_json_end(json);
 	LOGD("report(%d):%s\r\n", json->length, json->buff);
-
 	return json;
 }
 
@@ -136,18 +207,7 @@ int32_t calc_q(float p_b, float p_f, float angle)
 	return Q;
 }
 
-void sensor_data_clear(uint8_t item)
-{
-	for(int i=0; i<DP_MAX_NUMBER; i++) {
-		if(item>0 && item<STROE_MAX_NUMBER) {
-			dp_data[i].dt[item].v = 0.0;
-			dp_data[i].dt[item].t = 0;
-			dp_data[i].dt[item].isOk = true;
-		}
-	}
-}
-
-void record_sensor_data(time_t t)
+void record_sensor_data(time_t t, uint16_t record_item)
 {
 	extern float sensor_data[SAMPLE_ADC_CHANNEL];
 	uint8_t i = 0;
@@ -160,32 +220,58 @@ void record_sensor_data(time_t t)
 
 	Q = calc_q(p_b, p_f, angle);
 
-	sensor_data_clear(0);
 	for(i=0; i<SAMPLE_ADC_CHANNEL-1; i++)
 	{
-		dp_data[i].dt[0].v = sensor_data[i];
-		dp_data[i].dt[0].t = time;
-		dp_data[i].dt[0].isOk = true;
+		dp_data[i].dt[record_item].v = sensor_data[i];
+		dp_data[i].dt[record_item].t = time;
+		dp_data[i].dt[record_item].isOk = true;
 	}
-	dp_data[i].dt[0].v = at_device_get_rssi();
-	dp_data[i].dt[0].t = time;
-	dp_data[i].dt[0].isOk = true;
+	dp_data[i].dt[record_item].v = at_device_get_rssi();
+	dp_data[i].dt[record_item].t = time;
+	dp_data[i].dt[record_item].isOk = true;
 	i++;
-	dp_data[i].dt[0].v = Q;
-	dp_data[i].dt[0].t = time;
-	dp_data[i].dt[0].isOk = true;
+	dp_data[i].dt[record_item].v = Q;
+	dp_data[i].dt[record_item].t = time;
+	dp_data[i].dt[record_item].isOk = true;
 }
 
-int valve_control(void)
+int valve_control(uint16_t sample_count, SampleFreq sample_freq)
 {
+	uint16_t wakeupcount = sample_count;
+	uint8_t next_degree = 0;
+	uint8_t pre_degree = 0;
+	float angle = 0;
 
+	if(wakeupcount == 0)
+	{
+		pre_degree = angle_default_value[MAX_SAMPLE_ITEM(sample_freq) - 1];
+	}
+	else
+	{
+		pre_degree = angle_default_value[wakeupcount-1];
+	}
+	next_degree = angle_default_value[wakeupcount];
+	LOGD("%s:wakeupcount = %d, next degree = %d, pre_degree = %d\r\n",__FUNCTION__,wakeupcount, next_degree, pre_degree);
+
+	//open valve to degree
+	if(pre_degree != next_degree){
+		angle = (next_degree*90.0/100.0);
+		LOGD("%s:angle = %f\r\n", __FUNCTION__, angle);
+		//return moto_ctrl_for_angle(angle);
+		//need control moto for angle
+		return true;
+	}else{
+		LOGD("%s:nothing to do\r\n", __FUNCTION__);
+	}
+
+	return 0;
 }
 
 void run_process(void)
 {
-	char *clientid = "TEST001";
-	char *username = "448403";
-	char *passwd = "WIZpdLCOQv1cWYCbrKmwYzM5XeJ6ewZ9Ly0M7lrQLgs=";
+//	char *clientid = "TEST001";
+//	char *username = "448403";
+//	char *passwd = "WIZpdLCOQv1cWYCbrKmwYzM5XeJ6ewZ9Ly0M7lrQLgs=";
 	cavan_json_t *report_json = NULL;
 
 	if(!modem_init()) {
@@ -213,12 +299,20 @@ void DataProcessTask(void const * argument)
 		.subtype = 0x1234,
 		.u.buf = "567890",
 	};
+	uint16_t sample_count = 0;
+	float Q = 0.0;
 
 	// init nvitem
 	init_nvitems();
 
-	// init sensors param
+	// init sensors parama
 	Sensors_param_init();
+
+	//init mqtt account
+	init_mqtt_account();
+
+	//init device params
+	init_device_params();
 
 	// check boot mode
 	bootmode = device_get_bootmode();
@@ -246,8 +340,9 @@ void DataProcessTask(void const * argument)
 		modem_deinit();
 
 		while (1) {
-			next_time = SleepAndWakeUp(MIN_TO_SECONDS(5));
-
+			next_time = SleepAndWakeUp(MIN_TO_SECONDS(sample_freq));
+			osDelay(300);
+			sample_count = calc_wakeup_count(MIN_TO_SECONDS(sample_freq));
 			//send sensor sample message
 			os_msg_send(sensorsQueueHandle, &p_msg, 0);
 			//wait sample task complete
@@ -258,12 +353,14 @@ void DataProcessTask(void const * argument)
 			else
 			{
 				//record sensor data
-				record_sensor_data(next_time);
+				record_sensor_data(next_time, RECORD_SENSOR_ITEM(sample_count, sample_freq));
 				//valve control
-				valve_control();
-				//wait send task complete
-				LOGI("sensor sample ok, next task run\r\n");
-				run_process();
+				valve_control(sample_count, sample_freq);
+				//check whether update sensor data
+				if(IS_UPDATE_SENSOR_DATA(sample_count, sample_freq, update_freq)) {
+					LOGI("sensor sample ok, next task run\r\n");
+					run_process();
+				}
 			}
 			osDelay(100);
 		}
